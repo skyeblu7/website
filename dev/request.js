cbc6be6c70b0247e23c65ac598b86282d9f54735
skyeblu7 <skyeblu7@protonmail.com>
2020-01-19 15:29:51 -0600

    fixed pathname

--- dev/request.js
+++ dev/request.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/dev',function(res,req) {
-        req.sendFile("/home/ubuntu/test/home.html");
+        req.sendFile("/home/ubuntu/website/dev/home.html");
 })
 var port = 80;
 app.listen(port);
