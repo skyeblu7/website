78584273f0e84c84c3b6cf45008d02cb8c4a2e7c
skyeblu7 <skyeblu7@protonmail.com>
2020-01-19 15:35:57 -0600

    dev is through port 443

--- dev/request.js
+++ dev/request.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/dev',function(res,req) {
+app.get('/',function(res,req) {
         req.sendFile("/home/ubuntu/website/dev/home.html");
 })
-var port = 80;
+var port = 443;
 app.listen(port);
 console.log('Listening on port', port);
