ee7aeea4862308af8304e8e66de65576242d5a08
skyeblu7 <skyeblu7@protonmail.com>
2020-01-21 03:17:22 +0000

    removed commented code

--- dev/request.js
+++ dev/request.js
@@ -3,13 +3,15 @@ port = 443;
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
 
 app.post('/loginout', urlencodedParser, function(req, res){
-	res.render('contact', {qs: req.query});
+	res.render('loginout', {data: req.body});
+	console.log(req.body);
 });
 
 app.get('/', function(req,res){
@@ -36,117 +38,18 @@ app.get('/loginout', function(req,res){
 	res.render('loginout');
 });
 app.get('/myprofile/:name', function(req,res){
-	var data = {age: 69, job: 'ninja', hobbies: ['eating', 'sleeping', 'ece']}
-	res.render('myprofile', {person: req.params.name, data: data});
+	var personal = {age: 69, job: 'ninja', hobbies: ['eating', 'sleeping', 'ece']};
+	res.render('myprofile', {person: req.params.name, personal: data});
 });
 
 
 app.get('/ACM-sigarch-logo.png', function(req,res){
-	res.sendFile('/home/ubuntu/website/dev/assets/ACM-sigarch-logo.png');
+	res.sendFile(__dirname + '/assets/ACM-sigarch-logo.png');
 });
-
-
 
 
 app.listen(port);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var http = require('http');
-var fs = require('fs'); 
-var port = 443;
-
-
-var server = http.createServer(function (req, res) {
-	console.log('request was made: ' + req.url);
-
-	if(req.url === '/'){
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		var myReadStream = fs.createReadStream(__dirname + '/html/home.html');
-		myReadStream.pipe(res);
-	}
-	else if(req.url === '/image/ACM-sigarch-logo.png'){
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		fs.createReadStream(__dirname + req.url).pipe(res);
-	}
-	else{
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		var myReadStream = fs.createReadStream(__dirname + '/html' + req.url + '.html');
-		myReadStream.pipe(res);
-	}
-
-	
-	
-});
-
-server.listen(port); 
-
-console.log('Listening on port', port);
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-var server = http.createServer(function (req, res) {
-	fs.readFile('/home/ubuntu/website/dev/index.html', function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		res.end();
-	});
-})
-
-server.listen(port); 
-
-console.log('Listening on port', port);
-console.log(__dirname);
-
-*/
-
-/*
-var express = require('express');
-const path = require('path');
-var app = express();
-
-app.get('/',function(req,res) {
-        res.sendFile("/home/ubuntu/website/dev/home.html");
-})
-
-app.get('/test', (rq, rs) => {
-	rs.set('Content-Type', 'img/gif');
-	rs.sendFile(path.join(__dirname, 'images/test.gif'));
-})
-
-var port = 443;
-app.use(express.static(path.join(__dirname, 'images')));
-app.listen(port);
-console.log('Listening on port', port);
-console.log(__dirname)
-*/
 
